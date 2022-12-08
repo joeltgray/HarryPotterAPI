4e762a5d1f3902d1bc81d8713ce94bc8ffbd8023
Joel Gray <joeltgray@gmail.com>
2022-12-08 20:14:46 +0000

    try ix wsgi issues

--- app/api/wsgi.py
+++ app/api/wsgi.py
@@ -1,3 +1,17 @@
+import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/var/www/wizardingwords/venv/lib/python3.10/site-packages/')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/var/www/wizardingwords/app/api')
+
+# Activate your virtual env
+activate_env="/var/www/wizardingwords/venv/bin/activate.fish"
+execfile(activate_env, dict(__file__=activate_env))
+
 from app import create_app
 
 application = create_app()
