954c543b7aad17d7e097ff4980305b599b0ec968
Joel Gray <joeltgray@gmail.com>
2022-12-09 17:17:08 +0000

    Add encoding to json load

--- app/api/quote_parser.py
+++ app/api/quote_parser.py
@@ -14,7 +14,7 @@ def get_single_quote() -> Dict:
     
     try:
         # open the json file
-        with open(quote_path, "r") as quote_list:
+        with open(quote_path, "r", encoding='utf-8') as quote_list:
             # load the json data into a python dictionary
             data = json.load(quote_list)
     # except FileNotFoundError as e:
