c0898f8cf2245992c7cb5656a6a4a70442754a8d
Joel Gray <joeltgray@gmail.com>
2022-12-09 16:46:45 +0000

    add logging

--- app/api/quote_parser.py
+++ app/api/quote_parser.py
@@ -3,6 +3,7 @@ from typing import Dict
 import json
 import random
 from error_handling import GeneralException
+import logging
 
 data = None
 
@@ -14,21 +15,25 @@ def get_single_quote() -> Dict:
         with open(quote_path) as quote_list:
             # load the json data into a python dictionary
             data = json.load(quote_list)
-    except FileNotFoundError:
+    except FileNotFoundError as e:
             # Handle the FileNotFoundError exception
             print('The file does not exist.')
+            logging.info("Some sort of not found error error: ", e)
+            logging.info("'error': 'The file does not exist.', 'path': {}".format(quote_path))
             # Return a 400 error with the printed message as the json response
             return Response(
                 json.dumps({'error': 'The file does not exist.', 'path': quote_path}),
                 status=400,
                 mimetype='application/json'
             )
-    except ValueError:
+    except ValueError as e:
         # Handle the ValueError exception
         print('The file does not contain valid JSON data.')
+        logging.info("Some sort of value error: ", e)
+        logging.info("'error': 'The file does not contain valid JSON data.', 'path': {}".format(quote_path))
         # Return a 400 error with the printed message as the json response
         return Response(
-            json.dumps({'error': 'The file does not contain valid JSON data.'}),
+            json.dumps({'error': 'The file does not contain valid JSON data.', 'path': quote_path}),
             status=400,
             mimetype='application/json'
         )
