c9d9c55b90ab8b53485bc67ab63760a2a60c0c26
Joel Gray <joeltgray@gmail.com>
2023-04-20 10:50:46 +0100

    Add rate limiter for requests

--- app/app.py
+++ app/app.py
@@ -3,13 +3,21 @@ from flask_restful import Resource, Api, reqparse
 from error_handling import GeneralException
 from request_processor import Quote, QuoteByUUID
 from ww_api import WWAPI, WWAPIErrors
+from flask_limiter.util import get_remote_address
+from flask_limiter import Limiter
 
 def create_app() -> Flask:
 
+    # Error Handling
     errors = WWAPIErrors()
     errors.add_error(GeneralException, 400)
 
-    app = Flask("WizardingWords")
+    # Flask App and API
+    app = Flask("HarryPotterAPI")
+
+    # Rate Limiting
+    limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["50 per minute"])
+    
     api = WWAPI(error_list=errors, app=app)
 
     # APIS
